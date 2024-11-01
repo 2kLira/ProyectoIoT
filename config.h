d70de19ea2d377ff926f0647e1e7b1f18ac624dc
2kLira <160271956+2kLira@users.noreply.github.com>
2024-10-31 18:50:43 -0600

    Add files via upload

---
+++ config.h
@@ -0,0 +1,7 @@
+const char* ssid     = "Tec-IoT";
+const char* password = "spotless.magnetic.bridge";
+const char* hostname = "ESP8266_1";
+
+IPAddress ip(10, 22, 150, 161);
+IPAddress gateway(10, 22, 144, 1);
+IPAddress subnet(255, 255, 240, 0);
